a017d2c62dfddfcca1abb9ca8680832bf374b9ac
aseem <aseem.tim@gmail.com>
2022-11-02 19:24:54 -0400

    Deleted boilerplate and wrote starting code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome {props.name}</h1>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div>
+      <Header name="Aseem" />
+
+      <p>hehe</p>
+    </div>
+  );
+};
 
 export default App;
